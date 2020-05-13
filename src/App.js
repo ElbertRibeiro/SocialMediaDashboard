52d5007e2869e53c7ba5f494193c2cde50ee9b0d
ElbertRibeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-05-13 12:32:46 -0300

    update

--- src/App.js
+++ src/App.js
@@ -5,8 +5,22 @@ export default function App() {
   return (
     <div className="App">
       <div>
-        Social Media Dashboard
-        Total Followers: 23,005
+        <div>
+          Social Media Dashboard
+          Total Followers: 23,005
+        </div>
+
+        <label className="switch">
+          <input type="checkbox"/>
+          <span className="slider round"></span>
+        </label>
+      </div>
+
+      <div>
+        <div>Facebook</div>
+        <div>Twitter</div>
+        <div>Instagram</div>
+        <div>Youtube</div>
       </div>
     </div>
   );
